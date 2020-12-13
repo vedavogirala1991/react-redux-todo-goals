c16b013ce6d811bdde1c8bf51d652d18523857af
veda vogirala <veda.vogirala@gmail.com>
2020-12-14 00:40:09 +0530

    Redux Todo and Goal - Modifying indentation of the files

--- react-redux-todo-goals/index.js
+++ react-redux-todo-goals/index.js
@@ -1,27 +1,27 @@
 //Library Code
 function createStore(reducer) {
-    let state
-    let listeners = []
+  let state
+  let listeners = []
 
-    const getState = () => state
+  const getState = () => state
 
-    const subscribe = (listener) => {
-        listeners.push(listener)
-        return () => {
-            listeners.filter((l) => l !== listener)
-        }
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+        listeners.filter((l) => l !== listener)
     }
+  }
 
-    const dispatch = (action) => {
-        state = reducer(state, action)
-        listeners.forEach(listener => listener())
-    }
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
 
-    return {
-        getState,
-        subscribe,
-        dispatch,
-    }
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  }
 }
 
 //App Code
@@ -36,38 +36,38 @@ const REMOVE_GOAL = 'REMOVE_GOAL'
 
 //Action Creators
 function addTodoAction(todo) {
-    return {
-        type: ADD_TODO,
-        todo,
-    }
+  return {
+    type: ADD_TODO,
+    todo,
+  }
 }
 
 function removeTodoAction(id) {
-    return {
-        type: REMOVE_TODO,
-        id,
-    }
+  return {
+    type: REMOVE_TODO,
+    id,
+  }
 }
 
 function toggleTodoAction(id) {
-    return {
-        type: TOGGLE_TODO,
-        id,
-    }
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
 }
 
 function addGoalAction(goal) {
-    return {
-        type: ADD_GOAL,
-        goal,
-    }
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
 }
 
 function removeGoalAction(id) {
-    return {
-        type: REMOVE_GOAL,
-        id,
-    }
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
 }
 
 
@@ -76,57 +76,57 @@ const store = createStore(app)
 
 //Root Reducer that returns todas and goals reducers
 function app(state = {}, action) {
-    return {
-        todos: todos(state.todos, action),
-        goals: goals(state.goals,action),
-    }
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals,action),
+  }
 }
 
 //TODO's Reducer
 function todos (state = [], action) {
-    switch (action.type) {
-        case ADD_TODO:
-            return state.concat([action.todo])
-        case REMOVE_TODO:
-            return state.filter((todo) => todo.id !== action.id)
-        case TOGGLE_TODO:
-            return state.map(todo => todo.id !== action.id ? todo :
-                Object.assign({}, todo, { complete: !todo.complete }))
-        default:
-            return state
-    }
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo])
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id)
+    case TOGGLE_TODO:
+      return state.map(todo => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default:
+      return state
+  }
 }
 
 //Goals Reducer
 function goals (state = [], action) {
-    switch (action.type) {
-        case ADD_GOAL:
-            return state.concat([action.goal])
-        case REMOVE_GOAL:
-            return state.filter((goal) => goal.id !== action.id)
-        default:
-            return state
-    }
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal])
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
 }
 
 
 //Sample Dispatches on the store
 store.dispatch(addTodoAction({
-    id: 0,
-    name: 'Walk the dog',
-    complete: false,
+  id: 0,
+  name: 'Walk the dog',
+  complete: false,
 }))
 
 store.dispatch(addTodoAction({
-    id: 1,
-    name: 'Wash the car',
-    complete: false,
+  id: 1,
+  name: 'Wash the car',
+  complete: false,
 }))
 
 store.dispatch(addTodoAction({
-    id: 2,
-    name: 'Go to the gym',
-    complete: true,
+  id: 2,
+  name: 'Go to the gym',
+  complete: true,
 }))
 
 store.dispatch(removeTodoAction(1))
@@ -134,13 +134,13 @@ store.dispatch(removeTodoAction(1))
 store.dispatch(toggleTodoAction(0))
 
 store.dispatch(addGoalAction({
-    id: 0,
-    name: 'Learn Redux'
+  id: 0,
+  name: 'Learn Redux'
 }))
 
 store.dispatch(addGoalAction({
-    id: 1,
-    name: 'Lose 20 pounds'
+  id: 1,
+  name: 'Lose 20 pounds'
 }))
 
 store.dispatch(removeGoalAction(0))
